6f5d615be5dadbbe826204cd3c57c7199e053920
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-07-17 19:01:11 +0100

    Add background

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from "@/components/Hero";
 import PopularCharacters from "@/components/PopularCharacters";
-import FadeSection from "@/components/FadeInSection";
+import FadeSection from "@/components/FadeSection";
 
 export default function Home() {
     return (
         <div className="h-screen overflow-y-scroll scroll-snap-y scroll-snap-mandatory">
-        <section className="relative h-screen scroll-snap-start">
+        <section className="relative h-screen scroll-snap-start bg-gray-600 bg-[url('/hero-bg.jpg')] bg-blend-multiply">
             <FadeSection><Hero /></FadeSection>
         </section>
         <section className="relative h-screen scroll-snap-start bg-gray-900">
