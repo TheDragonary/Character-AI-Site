557db636e28a1f76eafc3c85c078850a31a166a0
TheDragonary <112120245+TheDragonary@users.noreply.github.com>
2025-07-18 02:17:15 +0100

    Add description

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import FadeSection from "@/components/FadeSection";
 export default function Home() {
     return (
         <div className="h-screen overflow-y-scroll scroll-snap-y scroll-snap-mandatory">
-        <section className="relative h-screen scroll-snap-start bg-gray-600 bg-[url('/hero-bg.jpg')] bg-blend-multiply">
+        <section className="relative h-screen scroll-snap-start bg-gray-700 bg-[url('/hero-bg.jpg')] bg-blend-multiply">
             <FadeSection><Hero /></FadeSection>
         </section>
         <section className="relative h-screen scroll-snap-start bg-gray-900">
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,9 +4,21 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 export default function Hero() {
     return (
         <section className="min-h-screen flex flex-col items-center justify-center">
-            <h1 className="text-6xl font-heading font-bold">Ch.ai Cafe</h1>
-            <p className="mt-4 text-2xl mb-4 font-accent">AI characters, brewed just for you</p>
-            <div className="flex gap-4 items-center flex-col sm:flex-row">
+            <h1 className="text-6xl font-heading font-bold">Ch.ai Café</h1>
+            <p className="mt-4 text-2xl font-accent">Brewing Bots & Banter</p>
+            <p className='font-accent text-xl text-gray-300 text-center md:text-center max-w-6xl mx-auto md:px-4 lg:px-0 mt-8 leading-relaxed'>
+                Unleash digital companionship at Ch.ai Café—chat with your favorite AI characters anytime,<br></br>whether on the site or through Discord. You can enjoy:
+            </p>
+                <ul className='list-disc list-inside font-accent mx-auto text-lg leading-relaxed space-y-1 mt-6 mb-6'>
+                    <li><strong>Seamless conversation continuity</strong> <em>- Your chats pick up where they left off, no matter the platform</em></li>
+                    <li><strong>Effortless character management</strong> <em>- Add characters to your profile and easily switch between them</em></li>
+                    <li><strong>Massive library of genres and personalities</strong> <em>- Chat with a wide range of unique characters</em></li>
+                    <li><strong>Discord integration</strong> <em>- Your chats and characters, fully synced with our Discord bot</em></li>
+                </ul>
+            <p className='font-accent text-xl text-gray-300 text-center md:text-center max-w-6xl mx-auto md:px-4 lg:px-0 mb-8 leading-relaxed'>
+                Dive in and discover what Ch.ai has to offer.
+            </p>
+            <div className="flex gap-4 items-center flex-col sm:flex-row mt-4">
                 <a
                     className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-sans font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
                     href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
